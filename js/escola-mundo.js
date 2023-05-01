57c23945981e7d8285591ed41177bd429df0566d
Ayrton Marinho <ayrtonmarinho07@gmail.com>
2023-05-01 17:51:11 -0300

    Update: some changes in root of the project.

--- js/escola-mundo.js
+++ js/escola-mundo.js
@@ -1,102 +1,145 @@
 function epiragonOn() {
-    epiragon = document.getElementById('epiragon')
-    novyatt = document.getElementById('novyatt')
-    siaahra = document.getElementById('siaahra')
-    thundaera = document.getElementById('thundaera')
-    yggbrasil = document.getElementById('yggbrasil')
-    glaciterno = document.getElementById('glaciterno')
-    epiragon.style.color = "red"
-    epiragon.style.display = "table"
-    novyatt.style.display = "none"
-    siaahra.style.display = "none"
-    thundaera.style.display = "none"
-    yggbrasil.style.display = "none"
-    glaciterno.style.display = "none"
+    epiragon = document.getElementById('epiragon');
+    mundo = document.getElementById('mundo');
+    allHide();
+    epiragon.style.display = "table";
+    mundo.style.display = "table";
 
 
 }
 
 function novyattOn() {
-    epiragon = document.getElementById('epiragon')
-    novyatt = document.getElementById('novyatt')
-    siaahra = document.getElementById('siaahra')
-    thundaera = document.getElementById('thundaera')
-    yggbrasil = document.getElementById('yggbrasil')
-    glaciterno = document.getElementById('glaciterno')
-
-    epiragon.style.display = "none"
-    novyatt.style.display = "table"
-    siaahra.style.display = "none"
-    thundaera.style.display = "none"
-    yggbrasil.style.display = "none"
-    glaciterno.style.display = "none"
+    novyatt = document.getElementById('novyatt');
+    mundo = document.getElementById('mundo');
+    allHide();
+    novyatt.style.display = "table";
+    mundo.style.display = "table";
 
 }
 
 function siaahraOn() {
-    epiragon = document.getElementById('epiragon')
-    novyatt = document.getElementById('novyatt')
-    siaahra = document.getElementById('siaahra')
-    thundaera = document.getElementById('thundaera')
-    yggbrasil = document.getElementById('yggbrasil')
-    glaciterno = document.getElementById('glaciterno')
-
-    epiragon.style.display = "none"
-    novyatt.style.display = "none"
-    siaahra.style.display = "table"
-    thundaera.style.display = "none"
-    yggbrasil.style.display = "none"
-    glaciterno.style.display = "none"
+    siaahra = document.getElementById('siaahra');
+    mundo = document.getElementById('mundo');
+    allHide();
+    siaahra.style.display = "table";
+    mundo.style.display = "table";
 
 }
 
 function thundaeraOn() {
-    epiragon = document.getElementById('epiragon')
-    novyatt = document.getElementById('novyatt')
-    siaahra = document.getElementById('siaahra')
-    thundaera = document.getElementById('thundaera')
-    yggbrasil = document.getElementById('yggbrasil')
-    glaciterno = document.getElementById('glaciterno')
-
-    epiragon.style.display = "none"
-    novyatt.style.display = "none"
-    siaahra.style.display = "none"
-    thundaera.style.display = "table"
-    yggbrasil.style.display = "none"
-    glaciterno.style.display = "none"
+    thundaera = document.getElementById('thundaera');
+    mundo = document.getElementById('mundo');
+    allHide();
+    thundaera.style.display = "table";
+    mundo.style.display = "table";
 
 }
 
 function yggbrasilOn() {
-    epiragon = document.getElementById('epiragon')
-    novyatt = document.getElementById('novyatt')
-    siaahra = document.getElementById('siaahra')
-    thundaera = document.getElementById('thundaera')
-    yggbrasil = document.getElementById('yggbrasil')
-    glaciterno = document.getElementById('glaciterno')
+    yggbrasil = document.getElementById('yggbrasil');
+    mundo = document.getElementById('mundo');
+    allHide();
+    yggbrasil.style.display = "table";
+    mundo.style.display = "table";
 
-    epiragon.style.display = "none"
-    novyatt.style.display = "none"
-    siaahra.style.display = "none"
-    thundaera.style.display = "none"
-    yggbrasil.style.display = "table"
-    glaciterno.style.display = "none"
 
 }
 
 function glaciternoOn() {
-    epiragon = document.getElementById('epiragon')
-    novyatt = document.getElementById('novyatt')
-    siaahra = document.getElementById('siaahra')
-    thundaera = document.getElementById('thundaera')
-    yggbrasil = document.getElementById('yggbrasil')
-    glaciterno = document.getElementById('glaciterno')
+    glaciterno = document.getElementById('glaciterno');
+    mundo = document.getElementById('mundo');
+    allHide();
+    glaciterno.style.display = "table";
+    mundo.style.display = "table";
+
+}
+
+function maeraOn() {
+    maera = document.getElementById('maera');
+    mundo = document.getElementById('mundo');
+    allHide();
+    maera.style.display = "table";
+    mundo.style.display = "table";
+
+}
+
+function elementaisOn() {
+    elementais = document.getElementById('elementais');
+    divindade = document.getElementById('divindade');
+    allHide();
+    elementais.style.display = "table";
+    divindade.style.display = "table";
+
+}
+
+function proibidasOn() {
+    proibidas = document.getElementById('proibidas');
+    divindade = document.getElementById('divindade');
+    allHide();
+    proibidas.style.display = "table";
+    divindade.style.display = "table";
+
+}
+
+function esquecidasOn() {
+    esquecidas = document.getElementById('esquecidas');
+    divindade = document.getElementById('divindade');
+    allHide();
+    esquecidas.style.display = "table";
+    divindade.style.display = "table";
+
+}
+
+function mundoOn() {
+    mapa = document.getElementById('map_img');
+    mundo = document.getElementById('mundo');
+    allHide();
+    mapa.style.display = "none";
+    mundo.style.display = "table";
+
+
+}
+
+function divindadeOn() {
+    mapa = document.getElementById('map_img');
+    divindade = document.getElementById('divindade');
+    allHide();
+    mapa.style.display = "none";
+    divindade.style.display = "table";
+
+
+}
+
+function defaultView() {
+    allHide();
+
+}
+
+function allHide() {
+    let epiragon = document.getElementById('epiragon')
+    let novyatt = document.getElementById('novyatt')
+    let siaahra = document.getElementById('siaahra')
+    let thundaera = document.getElementById('thundaera')
+    let yggbrasil = document.getElementById('yggbrasil')
+    let glaciterno = document.getElementById('glaciterno')
+    let maera = document.getElementById('maera');
+    let mundo = document.getElementById('mundo');
+    let divindade = document.getElementById('divindade')
+    let elementais = document.getElementById('elementais');
+    let proibidas = document.getElementById('proibidas');
+    let esquecidas = document.getElementById('esquecidas');
+
 
     epiragon.style.display = "none"
     novyatt.style.display = "none"
     siaahra.style.display = "none"
     thundaera.style.display = "none"
     yggbrasil.style.display = "none"
-    glaciterno.style.display = "table"
-
+    glaciterno.style.display = "none"
+    maera.style.display = "none";
+    mundo.style.display = "none";
+    divindade.style.display = "none";
+    elementais.style.display = "none";
+    proibidas.style.display = "none";
+    esquecidas.style.display = "none";
 }
